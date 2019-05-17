54d08303929ead9585dcca4cd333e7e76853aeed
HyunjaeLee <leehj8687@gmail.com>
2019-05-18 00:07:16 +0900

    Merge commit

--- roadmanager.py
+++ roadmanager.py
@@ -16,7 +16,7 @@ from openpyxl import load_workbook
 
 <Usage>
  Input example  :
-        python roadmanager.py <your_roadfile.csv> <sheet_name> <column_name>
+        python roadmanager.py <your_roadfile.xlsx> <sheet_name> <column_name>
  Output example :
         It will return (latitude, longitude) in the address
 '''
@@ -30,7 +30,6 @@ def main():
     your_roadfile = sys.argv[1]
     sheet_name = sys.argv[2]
     column_name = sys.argv[3]
-    start_row = sys.argv[4]
 
     # Change the address to (latitude, longitude)
     your_roadfile, sheet_name = returnColumn(
@@ -45,14 +44,15 @@ def changeAddress(geolocator, address):
 
     if location is None:
         print("No (latitude, longitude) is available at the address {}".format(
-            location))
-        quit()
+            address))
+        return 0, 0
 
-    return (location.latitude, location.longitude)
+    return location.latitude, location.longitude
 
 
 def checkInputs(checkInput):
-    if len(checkInput) != 5:
+    if len(checkInput) != 4:
+        print(checkInput)
         printError()
 
 
@@ -64,30 +64,19 @@ def returnColumn(path_your_roadfile, sheet_name,
     '''
     # If returntype is 1
     if returntype == 1:
-        geolocator = Nominatim(user_agent=" THIS IS ROAD MANAGER")
+        geolocator = Nominatim(user_agent=" THIS IS ROAD MANAGER", timeout=10)
 
     # <your_roadfile.csv> <sheet_name> <column_name>
-    # 1st check : if your_roadfile is vaild form
+
+    ''' 1st check '''
+    # if your_roadfile is vaild form
     if os.path.isfile(path_your_roadfile):
         print("Correct access")
     else:
         print("[ERROR] Failed to load Excel File : %s" % (path_your_roadfile))
 
-    # for fp in path_your_roadfile:
-    #     # Split the extension from the path and normalise it to lowercase.
-    #     ext = os.path.splitext(fp)[-1].lower()
-
-    #     # Now we can simply use == to check for equality, no need for wildcards.
-    #     if ext == ".csv":
-    #         print(fp, "is an csv!")
-    #     elif ext == ".xlsx":
-    #         print(fp, "is a xlsx file!")
-    #     else:
-    #         print(fp, "is an unknown file format.")
-    #         print(" We allow only .csv or .xlsx files ")
-    #         quit()
-
-    # 2nd check : if your_roadfile has the given 'sheet_name'
+    ''' 2nd check '''
+    # if your_roadfile has the given 'sheet_name'
     # Execl handler
     wb = load_workbook(filename=path_your_roadfile)
     if wb is None:
@@ -95,10 +84,12 @@ def returnColumn(path_your_roadfile, sheet_name,
 
     # get the worksheet
     worksheet = wb[sheet_name]
+    print(worksheet)
     if worksheet is None:
         printError()
 
-    # 3rd check : if the worksheet has the given 'column_name'
+    ''' 3rd check '''
+    # if the worksheet has the given 'column_name'
     # Check columns
     list_with_values = []
 
@@ -113,22 +104,62 @@ def returnColumn(path_your_roadfile, sheet_name,
         wb.close()
         return path_your_roadfile, sheet_name, column_name
 
-    # 4th check : Get the contents of the given column    column_num = list_with_values.index(column_name)
-    column_num = list_with_values.index(column_name)
-    print('{} is located at {} column'.format(column_name, column_num+1))
+    ''' 4th check '''
+    # Get the contents of the given column
 
-    column_contents = return_column_from_excel(path_your_roadfile, sheet_name,
-                                               column_num, startrow+1)
+    column_num = list_with_values.index(column_name) + 1
+    changed_latitude_list = list()
+    changed_longitude_list = list()
 
-    # 5th check : Write
-    for eachcell in column_contents:
-        # worksheet[eachcell] = changeAddress(geolocator, worksheet[eachcell])
-        print(worksheet[eachcell].value)
-        # print("after ", eachcell)
+    print('{} is located at {} column'.format(column_name, column_num))
+    print('worksheet.max_row :', worksheet.max_row)
+    # Get the value of the column and change it to coordinate value
 
+    for r in range(2, worksheet.max_row+1):
+
+        d = worksheet.cell(row=r, column=column_num)
+        changed_latitude, changed_longitude = changeAddress(
+            geolocator, d.value)
+
+        if d.value is None:
+            print('There is no value at {} row , {} column'.format(r, column_num))
+            break
+
+        changed_latitude_list.append(changed_latitude)
+        changed_longitude_list.append(changed_longitude)
+        print(r)
+        print(d.value)
+        print(changed_latitude, changed_longitude)
+
+    ''' 5th check '''
+    # Write each list to a new column
+    initialized_row = 2
+    initialized_col = len(list_with_values)+1
+
+    print('Changing Addresses is about to start !')
+    for each_latitude in changed_latitude_list:
+        worksheet.cell(row=initialized_row,
+                       column=initialized_col).value = each_latitude
+        initialized_row += 1
+
+    worksheet.cell(row=1, column=initialized_col).value = column_name + '_x'
+    initialized_col += 1
+    initialized_row = 2
+
+    for each_longitude in changed_longitude_list:
+        worksheet.cell(row=initialized_row,
+                       column=initialized_col).value = each_longitude
+        initialized_row += 1
+
+    worksheet.cell(row=1, column=initialized_col).value = column_name + '_y'
+
+    print('Changing Addresses is finished ! ')
+
+    # Return and save the file
     if returntype == 1:
         # save data as excel file
-        # wb.save(filename='converted_' + path_your_roadfile)
+        wb.save(filename='converted_address.xlsx')
+        wb.close()
         print(" Finish to save !")
         return path_your_roadfile, sheet_name
     else:
@@ -139,15 +170,6 @@ def printError():
     print("You have to follow input rule")
     print("python roadmanager.py <your_roadfile.csv> <sheet_name> <column_name>")
     quit()
-
-
-def return_column_from_excel(file_name, sheet_name, column_num, first_data_row):
-    wb = load_workbook(filename=file_name)
-    ws = wb[sheet_name]
-    min_col, min_row, max_col, max_row = (
-        column_num, first_data_row, column_num, ws.max_row)
-    wb.close()
-    return ws.iter_cols(min_col, min_row, max_col, max_row)
 
 
 if __name__ == "__main__":
