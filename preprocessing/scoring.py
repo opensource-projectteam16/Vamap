9b7e8aad8d7fd0217441e3eac682320c3407252d
sangminBangbada <ofhistory04@naver.com>
2019-05-22 23:21:22 +0900

    Test without Weight

--- preprocessing/scoring.py
+++ preprocessing/scoring.py
@@ -4,8 +4,8 @@ from preprocessing.dataproc import dataproc
 from math import radians, cos, sin, asin, sqrt
 
 ''' 
-<How to work>   
-<Usage> 
+<How to work>
+<Usage>
  Input example  : Scoring(coverage, userSet, roadsSet, othersSet)
 
                 roadsSet = {0.5 : [csv,sheet1,x,y,x1,y1,x2,y2,idth,5],...}
@@ -24,7 +24,11 @@ from math import radians, cos, sin, asin, sqrt
 
                 Pack : values() of Set
 
- '''
+'''
+'''
+시나리오
+'''
+
 
 
 class Scoring:
@@ -36,54 +40,55 @@ class Scoring:
 
     # 2개 들어갈때 Weight 조정하기
     def packingList(self, predata, Weight, mode):
-        print('2')
-        predata = predata[0]
-        predata = predata[0]
+        result = []
+        for i in range(0, len(predata)):
+            data = []
+            pre = predata[i]
+            pre = pre[0]
 
-        setindex = []
-        sx = []
-        sy = []
-        st_x = []
-        st_y = []
-        ed_x = []
-        ed_y = []
-        va = []
-        for data, index in zip(predata, range(0, len(predata))):
-            ps = data.dtype
-            if ps.names[0] == 'x':
-                sx.append(predata[index])
-            elif ps.names[0] == 'y':
-                sy.append(predata[index])
-            elif ps.names[0] == 'x1':
-                st_x.append(predata[index])
-            elif ps.names[0] == 'y1':
-                st_y.append(predata[index])
-            elif ps.names[0] == 'x2':
-                ed_x.append(predata[index])
-            elif ps.names[0] == 'y2':
-                ed_y.append(predata[index])
-            elif ps.names[0] == 'value':
-                va.append(predata[index])
+            setindex = []
+            sx = []
+            sy = []
+            st_x = []
+            st_y = []
+            ed_x = []
+            ed_y = []
+            va = []
+            for data, index in zip(pre, range(0, len(pre))):
+                ps = data.dtype
+                if ps.names[0] == 'x':
+                    sx.append(pre[index])
+                elif ps.names[0] == 'y':
+                    sy.append(pre[index])
+                elif ps.names[0] == 'x1':
+                    st_x.append(pre[index])
+                elif ps.names[0] == 'y1':
+                    st_y.append(pre[index])
+                elif ps.names[0] == 'x2':
+                    ed_x.append(pre[index])
+                elif ps.names[0] == 'y2':
+                    ed_y.append(pre[index])
+                elif ps.names[0] == 'value':
+                    va.append(pre[index])
+            sx = sx[0]
+            sy = sy[0]
+            if mode == 0:
+                for i, j in zip(sx, sy):
+                    result.append([float(i[0][0]), float(j[0][0]), 1.0])
+            if mode == 2 :
+                va = va[0]
+                for i, j, k in zip(sx, sy, va):
+                    pre = pre[0]
+                    result.append([float(i[0][0]), float(j[0][0]), 1.0])
+            if mode == 1:
+                st_x = st_x[0]
+                st_y = st_y[0]
+                ed_x = ed_x[0]
+                ed_y = ed_y[0]
+                for i, j, k, l, m, n in zip(sx, sy, st_x, st_y, ed_x, ed_y):
+                    result.append([float(i[0][0]), float(j[0][0]), float(k[0][0]), float(l[0][0]), float(m[0][0]), float(n[0][0]),1.0])
 
-        data = []
-        sx = sx[0]
-        sy = sy[0]
-        if mode == 0:
-            for i, j in zip(sx, sy):
-                data.append([float(i[0][0]), float(j[0][0]), 1])
-        if mode == 2 :
-            va = va[0]
-            for i, j, k in zip(sx, sy, va):
-                data.append([float(i[0][0]), float(j[0][0]), Weight[i]])
-        if mode == 1:
-            st_x = st_x[0]
-            st_y = st_y[0]
-            ed_x = ed_x[0]
-            ed_y = ed_y[0]
-            for i, j, k, l, m, n in zip(sx, sy, st_x, st_y, ed_x, ed_y):
-                data.append([float(i[0][0]), float(j[0][0]), float(k[0][0]), float(l[0][0]), float(m[0][0]), float(n[0][0]),Weight[i]])
-
-        return data
+        return result
 
     '''
 
@@ -102,8 +107,6 @@ class Scoring:
      '''
 
     def callObj(self):
-        print('1')
-
         #        Rcount = len(self.roadsSet)
         #        Ocount = len(self.othersSet)
         #        Ucount = len(self.userSet)
@@ -122,6 +125,10 @@ class Scoring:
         otherWeight = []
         uservalue = []
 
+        rpack =[]
+        opack = []
+
+
         for i in preroadW:
             a = i[2:]
             a = float(a)
@@ -130,7 +137,8 @@ class Scoring:
             a = i[2:]
             a = float(a)
             otherWeight.append(a)
-
+        x=[]
+        k=0
         if self.roadsSet != {}:
             for i in zip(self.roadsSet.values()):
                 a = i
@@ -139,67 +147,50 @@ class Scoring:
             for i in zip(self.othersSet.values()):
                 a = i
                 othersSetting.append([a])
-
-        print('146',othersSetting)
         mode = 0
-        if roadsSetting != []:
-            roadsSetting = roadsSetting[0]
-            roadsSetting = roadsSetting[0]
-            print('145', roadsSetting)
-            allR = dataproc(roadsSetting[0], mode=1)
-            labelR = allR.getdatalabel()
-            roadsPack = [roadsPack]
-            for i in range(0, len(labelR)):
-                roadsPack[i].append(allR.getdata())
-            roadsPack = roadsPack[0]
-            print('155', roadsPack)
-
-        if othersSetting != []:
-            othersSetting = othersSetting[0]
-            othersSetting = othersSetting[0]
-            allO = dataproc(othersSetting, mode=2)
-            labelO = allO.getdatalabel()
-            othersPack = [othersPack]
-            for i in range(0, len(labelO)):
-                othersPack[i].append(allO.getdata())
-            othersPack = othersPack[0]
 
         if userSetting != []:
             allU = dataproc(userSetting, mode=0)
             labelU = allU.getdatalabel()
-            userdata = [userPack]
+            #            userPack = [userPack]
             userPack.append(allU.getdata())
+            userPack = self.packingList(userPack, uservalue, 0)
 
-        # labelR = allR.getdatalabel()
-        # labelO = allO.getdatalabel()
-        # labelU = allU.getdatalabel()
+        if roadsSetting != []:
+            for i in range(0,len(roadsSetting)):
+                roadSheet = roadsSetting[i]
+                roadSheet = roadSheet[0]
+                roadSheet = roadSheet[0]
+                allR = dataproc(roadSheet, mode=1)
+                labelR = allR.getdatalabel()
+                for j in range(0, len(labelR)):
+                    roadsPack.append(allR.getdata())
+            rpack.append(self.packingList(roadsPack, roadWeight, 1))
 
-        # roadsPack = [roadsPack]
-        # othersPack = [othersPack]
-        # userdata = [userPack]
+        if othersSetting != []:
+            for i in range(0,len(othersSetting)):
+                otherSheet = othersSetting[i]
+                otherSheet = otherSheet[0]
+                allO = dataproc(otherSheet, mode=2)
+                labelO = allO.getdatalabel()
+                for j in range(0, len(labelO)):
+                    othersPack.append(allO.getdata())
+            opack.append(self.packingList(othersPack, otherWeight, 0))
 
-        # for i in range(0, len(labelR)):
-        #    roadsPack[i].append(allR.getdata())
-        # for i in range(0, len(labelO)):
-        #    othersPack[i].append(allO.getdata())
-        # userPack.append(allU.getdata())
-
-        # othersPack = othersPack[0]
-        # roadsPack = roadsPack[0]
-
+        return userPack, rpack, opack
+    '''
+        userPack = userPack[0]
         if userPack != []:
             userPack = self.packingList(userPack, uservalue, 0)
-        if roadsPack != []:
-            roadsPack = self.packingList(roadsPack, roadWeight, 1)
-        if othersPack != []:
-            othersPack = self.packingList(othersPack, otherWeight, 2)
-
-        return userPack, roadsPack, othersPack
+        if rpack != []:
+            roadsPack = self.packingList(rpack, roadWeight, 1)
+        if opack != []:
+            othersPack = self.packingList(opack, otherWeight, 2)
+    '''
 
     # newCoverage -> inCoverage -> converDis -> cal
 
     def newCoverage(self, userone):
-        print('4')
         x1 = userone[0] - self.coverage / 133330
         y1 = userone[1] + self.coverage / (133330 * cos(userone[0]))
         upPoint = [x1, y1]
@@ -210,8 +201,6 @@ class Scoring:
         return upPoint, downPoint
 
     def inCoverage(self, user, data):
-        print('5')
-
         upPoint, downPoint = self.newCoverage(user)
 
         incover = []
@@ -254,7 +243,6 @@ class Scoring:
         return incover
 
     def convertDis(self, userdata, data):
-        print('6')
         # convert decimal degrees to radians
 
         # user data간의 비교 판단
@@ -276,19 +264,16 @@ class Scoring:
 
     ## 가중치 매개변수 맞추기
     def cal_(self, user, data, coverage):
-        print('3')
         # value = weight*(distance/coverage)
         sum = 0;
         data = self.inCoverage(user, data)
 
         pre = self.convertDis(user, data)
-        print('282',pre)
 
         for i in range(0, len(pre)):
             x = data[i][2] * (pre[i] / coverage)
             sum += x
 
-        print('287',sum)
         return sum
 
     def cal_roads(self, user, data, coverage):
@@ -301,8 +286,8 @@ class Scoring:
         end = []
         for i in range(0, len(data)):
             middle.append([data[i][0], data[i][1]])
-            start.append(data[i][2])
-            end.append(data[i][3])
+            start.append([data[i][2],data[i][3]])
+            end.append([data[i][4],data[i][5]])
 
         middleD = self.convertDis(user, middle)
         startD = self.convertDis(user, start)
@@ -311,12 +296,12 @@ class Scoring:
         distantList = []
         pre = []
         for i in range(0, len(data)):
-            pre.append(middleD[i], startD[i], endD[i])
+            pre.append([middleD[i], startD[i], endD[i]])
             pre[i].sort()
             distantList.append([pre[i][0], data[i][6]])
 
-        for i in distantList:
-            x = data[i][6] * (i / coverage)
+        for i in range(0,len(distantList)):
+            x = data[i][6] * (distantList[i][0] / coverage)
             sum += x
             i += 1
 
@@ -324,6 +309,12 @@ class Scoring:
 
     def valueScore(self):
         user_data, roads_data, others_data = self.callObj()
+#        print('351',user_data)
+#        print('352',roads_data)
+#        print('353',others_data)
+        roads_data = roads_data[0]
+        others_data = others_data[0]
+
 
         resultUser = user_data
         count = 0
@@ -344,4 +335,13 @@ class Scoring:
         for i in range(0, len(others_data)):
             del others_data[i][2]
 
-        return resultUser, roads_data, others_data
+        print(len(resultUser))
+        print('\n===============Scored UserData=======================================================================================================================================\n')
+        print("[Latitude,  Longitude,  Value  ]")
+        for i in range(0, len(resultUser)):
+            if (2*(i+1)) > len(resultUser):
+                break
+            print(resultUser[int(len(resultUser)/(2*(i+1)))])
+        print("\n================================================================================================================================================================\n")
+
+        return resultUser, roads, others_data
