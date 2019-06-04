8001e92f0308cfd9aa46fc0c3c897b4215e0d416
HyunjaeLee <leehj8687@gmail.com>
2019-06-04 17:54:44 +0900

    Initiate basemap3D #5

---
+++ main/basemap3D.py
@@ -0,0 +1,18 @@
+from mpl_toolkits.basemap import Basemap
+from matplotlib import style
+import matplotlib.pyplot as plt
+import numpy as np
+
+''' This class is to draw 3D map using Basemap '''
+
+
+class Map3D:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def draw3dMap():
+        pass
+
+    def show3Dmap():
+        pass
